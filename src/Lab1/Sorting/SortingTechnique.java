86a4027c85180ee7f8777d34d61d91cb07d75c61
louay-hesham <louay.hesham@gmail.com>
2017-03-19 22:34:49 +0200

    updated skeleton

--- src/Lab1/Sorting/SortingTechnique.java
+++ src/Lab1/Sorting/SortingTechnique.java
@@ -1,22 +1,25 @@
 package Lab1.Sorting;
 
-import java.util.ArrayList;
-
 /**
  * Created by louay on 3/19/2017.
  */
 public abstract class SortingTechnique {
 
-    protected final ArrayList<Integer> array;
+    protected final int[] array;
 
     protected final SortingDirection direction;
 
-    public SortingTechnique(ArrayList<Integer> array, SortingDirection direction) {
+    public SortingTechnique(int[] array, SortingDirection direction) {
         this.array = array;
         this.direction = direction;
     }
 
-    public abstract ArrayList<Integer> getSortedArray();
+    public int[] getSortedArray() {
+        this.sort();
+        return this.array;
+    }
+
+    protected abstract void sort();
 
     protected boolean compareNumbers(int x, int y){
         if (this.direction.equals(SortingDirection.ASCENDING)) return x < y;
