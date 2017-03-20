0285a63301c29a5449fb10bb0d2170965f08561d
muhammadkorra <muhammadkorra@gmail.com>
2017-03-20 20:22:30 +0200

    added partition function (needs revision)

--- src/Lab1/Sorting/SortingImplementations/QuickSort.java
+++ src/Lab1/Sorting/SortingImplementations/QuickSort.java
@@ -2,6 +2,8 @@ package Lab1.Sorting.SortingImplementations;
 import Lab1.Sorting.SortingDirection;
 import Lab1.Sorting.SortingTechnique;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Muhammad Korra on 20-Mar-17.
  */
@@ -12,6 +14,29 @@ public class QuickSort extends SortingTechnique {
     public QuickSort(int[] array, SortingDirection direction) {
         super(array, direction);
         this.arraySize = array.length;
+    }
+
+    public int partition (int [] arr, int left, int right){
+        int pivot = arr[ThreadLocalRandom.current().nextInt(left, right+1)];
+
+            while (left <= right){
+                while (array[left] < pivot) left++;
+                while (array[right] > pivot) right++;
+                if (left <= right){
+                    swap(arr,left,right);
+                    left++;
+                    right--;
+                }
+            }
+
+            return left;
+    }
+
+    private static void swap (int [] array, int firstIndex, int secondIndex){
+        int temp;
+        temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
     }
 
     @Override
