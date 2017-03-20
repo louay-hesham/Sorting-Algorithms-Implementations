feb6a5db041b4e8d86f811193ecb6b04c7c6a7f4
muhammadkorra <muhammadkorra@gmail.com>
2017-03-20 18:12:47 +0200

    added constructor

--- src/Lab1/Sorting/SortingImplementations/QuickSort.java
+++ src/Lab1/Sorting/SortingImplementations/QuickSort.java
@@ -5,7 +5,22 @@ import Lab1.Sorting.SortingTechnique;
 /**
  * Created by Muhammad Korra on 20-Mar-17.
  */
-public class QuickSort {
+public class QuickSort extends SortingTechnique {
 
+    private int arraySize;
 
+    public QuickSort(int[] array, SortingDirection direction) {
+        super(array, direction);
+        this.arraySize = array.length;
+    }
+
+    @Override
+    protected void sort() {
+
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
 }
