02c8f759a206054f2c08f7ef02ba9042838d74ac
muhammadkorra <muhammadkorra@gmail.com>
2017-03-20 14:13:37 +0200

    Created QuickSort class file

---
+++ src/Lab1/Sorting/SortingImplementations/QuickSort.java
@@ -0,0 +1,11 @@
+package Lab1.Sorting.SortingImplementations;
+import Lab1.Sorting.SortingDirection;
+import Lab1.Sorting.SortingTechnique;
+
+/**
+ * Created by Muhammad Korra on 20-Mar-17.
+ */
+public class QuickSort {
+
+
+}
