0b95f5817420e80b4822807821b2e8bae10088c5
muhammadkorra <muhammadkorra@gmail.com>
2017-03-26 20:15:49 +0200

    QuickSort finished, passed all test cases

--- src/Lab1/Sorting/SortingImplementations/QuickSort.java
+++ src/Lab1/Sorting/SortingImplementations/QuickSort.java
@@ -14,14 +14,46 @@ public class QuickSort extends SortingTechnique {
         super(array, direction);
     }
 
+
+
     @Override
     protected void sort() {
-
+        quickSort(this.array,0,this.array.length - 1);
     }
 
     @Override
     public String toString() {
-        return null;
+        return "Quick Sort";
+    }
+
+    public static int partition (int [] array, int left, int pivot){
+        int x, i;
+        x = array[pivot];
+        i = left - 1;
+        for (int j = left; j <= pivot - 1; j++){
+            if (array[j] <= x){
+                i++;
+                swap(array,i ,j);
+            }
+
+        }
+
+        swap(array, i+1, pivot);
+        return (i+1);
+    }
+    public static void quickSort (int [] array, int left, int pivot){
+        if (left < pivot){
+            int q = partition(array,left,pivot);
+            quickSort(array, left, q - 1);
+            quickSort(array, q+1,pivot);
+        }
+    }
+
+    public static void swap (int [] array, int source, int destination){
+        int temp;
+        temp = array[destination];
+        array[destination] = array[source];
+        array[source] = temp;
     }
 }
 
