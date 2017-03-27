c44f8c2b39bc5b499e201f3d4bd2ad7b485b42af
muhammadkorra <muhammadkorra@gmail.com>
2017-03-27 18:47:02 +0200

    Created InsertionSort class

---
+++ src/Lab1/Sorting/SortingImplementations/InsertionSort.java
@@ -0,0 +1,25 @@
+package Lab1.Sorting.SortingImplementations;
+
+import Lab1.Sorting.SortingDirection;
+import Lab1.Sorting.SortingTechnique;
+
+/**
+ * Created by Muhammad Korra on 27-Mar-17.
+ */
+public class InsertionSort extends SortingTechnique{
+    public InsertionSort(int[] array, SortingDirection direction) {
+        super(array, direction);
+    }
+
+    @Override
+    protected void sort() {
+
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    
+}
